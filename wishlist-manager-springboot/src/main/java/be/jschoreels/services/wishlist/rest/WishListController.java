212eddf7d4ea48a40495470e88a4f438c82f4540
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2017-05-23 11:31:08 +0200

    Implements a simple rest service using Spring Boot, based on official guide.

---
+++ wishlist-manager-springboot/src/main/java/be/jschoreels/services/wishlist/rest/WishListController.java
@@ -0,0 +1,32 @@
+package be.jschoreels.services.wishlist.rest;
+
+import be.jschoreels.services.wishlist.api.domain.WishList;
+import be.jschoreels.services.wishlist.core.domain.DefaultWishList;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by jschoreels on 23.05.17.
+ */
+@RestController
+public class WishListController {
+
+    private final AtomicLong counter = new AtomicLong(0l);
+
+    /**
+     *
+     * @param name the name of the wishlist to return. by default, "all" is used
+     * @return a stub wishlist to test rest service. Will be linked to persistence later.
+     */
+    @RequestMapping(value = "wishlist", method = RequestMethod.GET)
+    public WishList wishList (@RequestParam(value = "name", defaultValue = "all") String name){
+        return DefaultWishList.create()
+            .withId(String.valueOf(counter.incrementAndGet()))
+            .withName(name)
+            .now();
+    }
+}
